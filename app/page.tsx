2341352f59fc9a0347a24d484d6b4810ab09ce27
Emanuel Tavares <Morche17@proton.me>
2023-05-24 12:09:47 -0700

    Ahora se puede colocar la edad y se notifica si es mayor o menor de edad

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ import { useState } from "react"
 
 const Page = () => {
 
-  const [nombre,setNombre] = useState('Emanuel Alejandro T. M.');
+  const [nombre,setNombre] = useState('');
+  const [edad,setEdad] = useState(0);
+  const [mensaje,setMensaje] = useState('');
+
   const [contador, setContador] = useState(0);
 
   function contar(){
@@ -15,13 +18,38 @@ const Page = () => {
     setContador(0);
   }
   
+  const manejadorFormulario = (e:any) => {
+    e.preventDefault();
+
+    if (edad >= 18){
+      setMensaje('Es mayor de edad');
+    } else {
+      setMensaje('Es menor de edad');
+    }
+    console.log('Se hizo click');
+  }
+  
   return (
     <div>
       <h1>Proyecto final</h1>
       <h2>Estructura de datos</h2>
-      <h3>{nombre}</h3>
+      <h3>{mensaje}</h3>
       <button onClick={contar}>Contar</button>
       <button onClick={reiniciar}>Reiniciar</button>
+
+      <form 
+        onSubmit={manejadorFormulario}
+        >
+
+        <input type="text" 
+          placeholder="Edad" 
+          value={edad} 
+          onChange={(event) => setEdad(parseInt(event.target.value))}
+          />
+
+        <input type="submit" />
+      </form>
+      
       <p>{contador} número de clicks</p>
     </div>
   )
